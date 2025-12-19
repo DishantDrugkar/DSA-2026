993ef95186fa5df9741561044473fac9d840ff06
DishantDrugkar <dishantdrugkar4@gmail.com>
2025-12-19 21:39:25 +0530

    Selection sort

---
+++ src/src/main/java/org/example/striver/sorting/SelectionSort.java
@@ -0,0 +1,27 @@
+package org.example.striver.sorting;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int arr[] = {5,1,4,2,8};
+        int n = arr.length;
+        for(int i=0; i<n-1; i++){
+            int minIndex = i;
+            for(int j=i+1; j<n; j++){
+                if(arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+
+        }
+        printArray(arr);
+
+    }
+    public static void printArray(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
