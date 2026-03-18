054ac60b51f93624f77ed42ee6b3dcc2f7562abe
DishantDrugkar <dishantdrugkar4@gmail.com>
2026-03-18 22:54:00 +0530

    Reverse a LinkedList

--- src/src/main/java/org/example/implementation/linkedlist/ReverseLinkedList.java
+++ src/src/main/java/org/example/implementation/linkedlist/ReverseLinkedList.java
@@ -1,4 +1,67 @@
 package org.example.implementation.linkedlist;
 
 public class ReverseLinkedList {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void printLL(){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void reverseLL(){
+        Node prevNode = head;
+        Node currNode = head.next;
+        while(currNode != null){
+            Node nextNode = currNode.next;
+            currNode.next = prevNode;
+
+            prevNode =currNode;
+            currNode = nextNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+    public static void main(String[] args){
+        ReverseLinkedList list = new ReverseLinkedList();
+        list.addFirst(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printLL();
+        list.reverseLL();
+        list.printLL();
+    }
 }
