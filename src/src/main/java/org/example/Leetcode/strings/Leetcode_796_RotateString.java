07f8527979eae684d05ca646d27d5eeea5f34403
DishantDrugkar <dishantdrugkar4@gmail.com>
2026-04-05 20:00:25 +0530

    Rotate String

---
+++ src/src/main/java/org/example/Leetcode/strings/Leetcode_796_RotateString.java
@@ -0,0 +1,15 @@
+package org.example.Leetcode.strings;
+
+public class Leetcode_796_RotateString {
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        System.out.println(rotateString(s,goal));
+    }
+    public static boolean rotateString(String s, String goal){
+        if(s.length() != goal.length()){
+            return false;
+        }
+        return (s + s).contains(goal);
+    }
+}
